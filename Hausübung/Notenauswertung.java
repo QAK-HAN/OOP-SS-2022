8e3315f2f818399ec54cfef5fe6a1e94f6b4239b
QAK-HAN <qaweem@live.de>
2022-06-05 22:36:30 +0200

    Monty´s Patch (bis jetzt)

--- Hausübung/Notenauswertung.java
+++ Hausübung/Notenauswertung.java
@@ -4,16 +4,31 @@ import java.util.Scanner;
 public class Notenauswertung {
 
     public static void main(String[] args) throws FileNotFoundException {
-        int[][] hauptArray = new int[100][2];
-        keinPlan(hauptArray);
-        lel(hauptArray);
+        int zähler;
+        zähler = zählen();
+        int[][] hauptArray = new int[zähler][2];
+        hauptArray = csvAuslesung(hauptArray);
 
+    }
+
+    public static int zählen() throws FileNotFoundException {
+
+        File getCSVFiles = new File("Notenliste.csv");
+        Scanner sc = new Scanner(getCSVFiles);
+
+        int h = 0;
+        while (sc.hasNext()) {
+            sc.nextLine();
+
+            h++;
         }
+        sc.close();
+        return h;
 
-    
+    }
 
-    public static void keinPlan(int doppelFuge [][] ) throws FileNotFoundException {
-        
+    public static int[][] csvAuslesung(int doppelFuge[][]) throws FileNotFoundException {
+
         File getCSVFiles = new File("Notenliste.csv");
         Scanner sc = new Scanner(getCSVFiles);
 
@@ -22,25 +37,24 @@ public class Notenauswertung {
 
             String wischmopp = sc.nextLine();
             String[] mrPropper = wischmopp.split(",");
-            
+
             int fugeEins = Integer.parseInt(mrPropper[0]);
             int fugeZwei = Integer.parseInt(mrPropper[1]);
-            
+
             doppelFuge[h][0] = fugeEins;
             doppelFuge[h][1] = fugeZwei;
-            
-            h++;
 
+            h++;
 
         }
         sc.close();
-    } 
-    public static void lel (int[][] lul){
-        System.out.println(lul[0][0]);
+        return doppelFuge;
     }
 
+    public static void notenspiegel (){
+        int [] noten = new 
 
-
-
-
+        
     }
+
+}
